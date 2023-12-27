6c046999d6e337e28b25f70a17d55e5d18e68573
isrkan <111886585+isrkan@users.noreply.github.com>
2023-12-27 16:37:16 +0100

    Adding use of nulls and dates

--- Java/OOP/src/ClassesAndObjects/Main.java
+++ Java/OOP/src/ClassesAndObjects/Main.java
@@ -1,14 +1,18 @@
+import java.time.LocalDateTime;
+
 public class Main {
     public static void main(String[] args) {
 
         // Creating an object
-        Product myProduct = new Product("Milk", 2.49, 123);
+        LocalDateTime productionDate = LocalDateTime.of(2023, 1, 1, 10, 0); // Example production date
+        Product myProduct = new Product("Milk", 2.49, 123, "Tnuva Inc.", productionDate);
         myProduct.displayInfo();
 
         // Modifying object attributes
         myProduct.name = "Cheese";
         myProduct.price = 4.99;
+        myProduct.manufacturer = null;
+        myProduct.productionDate = LocalDateTime.now();
         myProduct.displayInfo();
-
     }
 }
--- Java/OOP/src/ClassesAndObjects/Product.java
+++ Java/OOP/src/ClassesAndObjects/Product.java
@@ -1,18 +1,26 @@
+import java.time.LocalDateTime;
+
 public class Product {
     // Fields
     String name;
     double price;
     final int productId; // Using for a constant field
+    String manufacturer; // Nullable field
+    LocalDateTime productionDate;
 
     // Constructor
-    public Product(String name, double price, int productId) {
+    public Product(String name, double price, int productId, String manufacturer, LocalDateTime productionDate) {
         this.name = name;
         this.price = price;
         this.productId = productId;
+        this.manufacturer = manufacturer;
+        this.productionDate = productionDate;
     }
 
     // Method
     public void displayInfo() {
-        System.out.println("Product ID: " + productId + ", Product: " + name + ", Price: $" + price);
+        System.out.println("Product ID: " + productId + ", Product: " + name + ", Price: $" + price
+                + ", Manufacturer: " + (manufacturer != null ? manufacturer : "N/A")
+                + ", Production date: " + (productionDate != null ? productionDate : "N/A"));
     }
 }
