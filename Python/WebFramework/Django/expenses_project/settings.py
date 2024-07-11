dcdef05be92b6dac69709d22f9d939a9c65e7156
isrkan <111886585+isrkan@users.noreply.github.com>
2024-07-11 21:24:40 +0200

    Adding use of mysql

--- Python/WebFramework/Django/expenses_project/settings.py
+++ Python/WebFramework/Django/expenses_project/settings.py
@@ -76,17 +76,26 @@ WSGI_APPLICATION = "expenses_project.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "expenses",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres1",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
+        "ENGINE": "django.db.backends.mysql",
         "NAME": "expenses",
-        "USER": "postgres",
-        "PASSWORD": "postgres1",
+        "USER": "root",
+        "PASSWORD": "mysql1",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "3306",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
